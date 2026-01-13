f06b4d36568e589e035aebfae0cd232d029f716f
Gustavo Gurgel <gurgelg15@gmail.com>
2026-01-13 18:39:33 -0300

    FEAT: Add API asset_bundle routes.

--- src/api.py
+++ src/api.py
@@ -1,9 +1,16 @@
 # fastapi dev api.py --host "::" --port 8000
 from vector_db import query_vector_store
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from models import AssetBundle
-from asset_generator import AssetsGenerator, load_zombie_souls_asset_bundle
+from asset_generator import AssetsGenerator
+from db import (
+    find_all_assets_bundles,
+    find_bundle_data_by_id,
+    delete_asset_bundle_by_id,
+)
+from typing import Any, Dict
+
 
 app = FastAPI()
 
@@ -12,8 +19,42 @@ class MapDescription(BaseModel):
     map_description: str
 
 
-@app.post("/generate-asset-bundle/")
-async def generate_asset_bundle(map_description: MapDescription) -> AssetBundle:
-    asset_generator = AssetsGenerator(map_description)
+@app.post("/asset-bundle/")
+async def route_post_asset_bundle(map_description: MapDescription) -> AssetBundle:
+    try:
+        asset_generator: AssetsGenerator = AssetsGenerator(
+            map_description.map_description
+        )
+        asset_bundle: AssetBundle = asset_generator.generate_asset_bundle()
+        return asset_bundle
+    except:
+        raise HTTPException(status_code=500, detail="Internal Server Error.")
 
-    return load_zombie_souls_asset_bundle()
+
+@app.get("/asset-bundle/")
+async def route_find_all_asset_bundle() -> list[Dict[str, Any]]:
+    return find_all_assets_bundles()
+
+
+@app.get("/asset-bundle/{id}")
+async def route_find_bundle_data_id(id: int) -> AssetBundle:
+    asset_bundle = find_bundle_data_by_id(id)
+
+    if asset_bundle == None:
+        raise HTTPException(
+            status_code=404, detail=f"Asset bundle with id {id} no found."
+        )
+
+    return asset_bundle
+
+
+@app.delete("/asset-bundle/{id}")
+async def route_delete_bundle_data_id(id: int):
+    was_delete = delete_asset_bundle_by_id(id)
+
+    if not was_delete:
+        return HTTPException(
+            status_code=404, detail=f"Asset bundle with id {id} no found."
+        )
+    else:
+        return Response(content=f"Asset bundle with id {id} was deleted.")
